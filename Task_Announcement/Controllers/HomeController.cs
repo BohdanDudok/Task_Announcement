f213603ca76725efb370e20dfa6b4b86bc7cce27
BohdanDudok <bodiadudok19@gmail.com>
2020-07-11 20:05:19 +0300

    Create buttons: Add, Edit, Delete.

--- Task_Announcement/Controllers/HomeController.cs
+++ Task_Announcement/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Task_Announcement.Models;
 
@@ -16,9 +17,68 @@ namespace Task_Announcement.Controllers
         {
             db = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(db.Announces.ToList());
+            return View(await db.Announces.ToListAsync());
+        }
+        //Create announcement
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Announce user)
+        {
+            db.Announces.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        //Edit announcement
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Announce user = await db.Announces.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Announce user)
+        {
+            db.Announces.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        //Delete announcement
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Announce user = await db.Announces.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Announce user = await db.Announces.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                {
+                    db.Announces.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
         }
     }
 }
