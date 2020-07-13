5db4e828c4255204064f4e51639eaf9400ccf72c
BohdanDudok <bodiadudok19@gmail.com>
2020-07-13 21:28:19 +0300

    Add search

--- Task_Announcement/Controllers/HomeController.cs
+++ Task_Announcement/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Task_Announcement.Models;
@@ -17,9 +19,38 @@ namespace Task_Announcement.Controllers
         {
             db = context;
         }
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await db.Announces.ToListAsync());
+            var disp = db.Announces.ToList();
+            return View(disp);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Filter(string title, string description, string data_added )
+        {
+            ViewData["title_an"] = title;
+            ViewData["description"] = description;
+            ViewData["data"] = data_added;
+
+            var empquery = from x in db.Announces select x;
+
+            if (!String.IsNullOrEmpty(title))
+            {
+                empquery = empquery.Where(x => x.Title.Contains(title));
+            }
+            if (!String.IsNullOrEmpty(description))
+            {
+                empquery = empquery.Where(x => x.Description.Contains(description));
+            }
+            if (!String.IsNullOrEmpty(data_added))
+            {
+                empquery = empquery.Where(x => x.data.Contains(data_added));
+            }
+            if(!String.IsNullOrEmpty(title) && !String.IsNullOrEmpty(description))
+            {
+                empquery = empquery.Where(x => x.Title.Contains(title) && x.Description.Contains(description));
+                return View(await empquery.AsNoTracking().ToListAsync());
+            }
+            return View(await empquery.AsNoTracking().ToListAsync());
         }
         //Create announcement
         public IActionResult Create()
---
+++ Task_Announcement/Models/AnnounceViewList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Announcement.Models
+{
+    public class AnnounceViewList
+    {
+        public IEnumerable<Announce> Announces { get; set; }
+        public int Id { set; get; }
+        public string Title { get; set; }
+        public string Description { get; set;}
+        public string Data_Added { get; set; }
+    }
+}
